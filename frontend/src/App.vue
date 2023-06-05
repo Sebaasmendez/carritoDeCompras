291b79f6feb22b392d8818e6ba7e80b36c6b426c
Sobrino Julian <sobrinojulian@protonmail.com>
2023-06-05 17:48:00 -0300

    refactor navbar (no esta terminado)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import MenuView from "./views/MenuView.vue";
+import Menu from './components/Menu.vue';
 import PieView from "./views/PieView.vue";
 </script>
 
 <template>
   <div class="d-flex flex-column min-vh-100">
-    <MenuView></MenuView>
+    <Menu />
     <RouterView />
     <PieView></PieView>
   </div>
